8ff0ed7d05472911142c17e3c187d2d428fc4a69
yalyzak <yaly.zak@gmail.com>
2026-07-01 22:22:45 +0300

    //

--- bindings.cpp
+++ bindings.cpp
@@ -1,5 +1,9 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
 #include "Vector3.h"
+#include "GameObject.h"
+#include "World.h"
 
 namespace py = pybind11;
 
@@ -10,5 +14,13 @@ PYBIND11_MODULE(bereshitCore, m) {
         .def_readwrite("x", &Vector3::x)
         .def_readwrite("y", &Vector3::y)
         .def_readwrite("z", &Vector3::z);
-}
 
+    py::class_<GameObject>(m, "GameObject")
+    .def(py::init<>())
+    .def_readwrite("name", &GameObject::name);
+
+    py::class_<World>(m, "World")
+    .def(py::init<std::list<GameObject*>>())
+    .def_readwrite("children", &World::children);
+
+}
