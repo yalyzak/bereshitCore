d806ffe516835992806e869d02ffeb1c1891be2b
yalyzak <yaly.zak@gmail.com>
2026-07-02 17:39:40 +0300

    //

--- GameObject.cpp
+++ GameObject.cpp
@@ -6,7 +6,7 @@
 #include "Transform.h"
 GameObject::GameObject() : transform(Transform()) {}
 
-GameObject::GameObject(Vector3 position, Vector3 rotation, Vector3 scale) : transform(position, rotation, scale) {}
+GameObject::GameObject(Vector3 position, Vector3 rotation, Vector3 scale, std::list<GameObject*> children) : transform(position, rotation, scale), children(children) {}
 
 void GameObject::AddComponent(Component* comp) {
     components.push_back(comp);
--- GameObject.h
+++ GameObject.h
@@ -33,7 +33,7 @@ class GameObject {
 
         std::list<Component*> components;
         GameObject();
-        GameObject(Vector3 position, Vector3 rotation, Vector3 scale);
+        GameObject(Vector3 position, Vector3 rotation, Vector3 scale, std::list<GameObject*>);
 
         void AddComponent(Component* comp);
         void setParent(GameObject* p) {
