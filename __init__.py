91c29563514f83286519dd08457ac086094c668e
Yaly Zak <yaly.zak@gmail.com>
2026-07-14 20:42:41 +0300

    //

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 import sys
+from pathlib import Path
 
-sys.path.append(r"C:/Users/yaly/PycharmProjects/bereshit/bereshit/bereshitCore/build/Release")
+base = Path(__file__).resolve().parent
+core_folder = base / "build" / "Release"
+
+sys.path.append(str(core_folder))
+
 import bereshitCore
 from bereshitCore import GameObject
 from bereshitCore import Component
 from bereshitCore import Vector3
 from bereshitCore import World
 from bereshitCore import Rigidbody
+from bereshitCore import Collider
 
 
