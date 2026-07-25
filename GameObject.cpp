3d25ef49c89ff63c779ca53803942ebbd0ef8695
Yaly Zak <yaly.zak@gmail.com>
2026-07-25 18:36:59 +0300

    //

--- GameObject.cpp
+++ GameObject.cpp
@@ -9,7 +9,7 @@
 
 
 GameObject::GameObject(Vector3 position, Vector3 rotation, Vector3 scale,
-                       const std::vector<GameObject*>& children, std::string name) : transform(cache, position, rotation, scale), children(children), name(name) {}
+                       const std::vector<GameObject*>& children, std::string name) : transform(*this, cache, position, rotation, scale), children(children), name(name) {}
 
 
 GameObject* GameObject::AddComponent(Component* comp) {
