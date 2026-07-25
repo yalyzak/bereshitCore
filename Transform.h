299e72c61a81d1335c74b99eb4ed388b5d92f395
Yaly Zak <yaly.zak@gmail.com>
2026-07-25 09:10:38 +0300

    //

--- Transform.h
+++ Transform.h
@@ -13,12 +13,30 @@ class Transform {
         Vector3 rotation;
         Vector3 scale;
         Quaternion quaternion;
+
+        [[nodiscard]] Vector3 GetLocalPosition() const {
+            return localPosition;
+        }[[nodiscard]] Vector3 GetLocalRotation() const {
+            return localRotation;
+        }[[nodiscard]] Vector3 GetLocalScale() const {
+            return localScale;
+        }
+
+        void SetLocalPosition(const Vector3& vec) {
+            localPosition = vec;
+        }void SetLocalRotation(const Vector3& vec) {
+            localRotation = vec;
+        }void SetLocalScale(const Vector3& vec) {
+            localScale = vec;
+        }
+
+
         Transform(const Vector3& position = Vector3(), const Vector3& rotation = Vector3(),const Vector3& scale = Vector3(), const Quaternion& quaternion = Quaternion());
 
     private:
-        Vector3 local_position;
-        Vector3 local_rotation;
-        Vector3 local_scale;
+        Vector3 localPosition;
+        Vector3 localRotation;
+        Vector3 localScale;
 };
 
 
