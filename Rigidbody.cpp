f3a876ea39f79431ef2fa065f40bac54cdcc113d
Yaly Zak <yaly.zak@gmail.com>
2026-07-28 19:47:15 +0300

    //

--- Rigidbody.cpp
+++ Rigidbody.cpp
@@ -255,8 +255,6 @@ void Rigidbody::SolveImpulse(Rigidbody &rb1, Rigidbody &rb2, const Vector3& cont
         PositionalCorrection(rb1, rb2, penetration, normal, inverseMass);
 
         ApplyImpulsePair(rb1, rb2, normal * J, r1, r2);
-
-        // Rigidbody::SolveFrictionImpulse(rb1, rb2, contact_point, normal, dt);
 }
 
 double Rigidbody::FindRestitution(const Rigidbody &rb1, const Rigidbody &rb2, double normalVelocity) {
