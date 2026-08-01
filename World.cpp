227ebd050080ff2c00aabc85186158ceda730ac5
yalyzak <yaly.zak@gmail.com>
2026-08-01 10:57:58 +0300

    //

--- World.cpp
+++ World.cpp
@@ -275,8 +275,6 @@ void World::Update(bool updateComponen) {
     }
     const auto& PhysicsChildren = GetAllChildrenPhysics();
 
-    CallChildrenUpdate(PhysicsChildren, tick, &Component::PhysicsUpdate);
-
     ApplyGravityToAll(gravity);
 
     auto collections = SolveCollectionsFirstIteration(GetAllColliders(), tick);
@@ -284,7 +282,9 @@ void World::Update(bool updateComponen) {
     for (int i =0; i < physics_epochs + 1; i++) {
         SolveCollections(collections, tick);
         SolveJoints(GetAllJoints(), tick);
+        CallChildrenUpdate(PhysicsChildren, tick, &Component::PhysicsUpdate);
     }
+
     IntegrateAll(tick);
 }
 
